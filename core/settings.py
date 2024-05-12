ff18437c17dce8e2ea9d9f6f3399e3a3f74334c2
skills nwokolo anthony <skillsnwokolo372@gmail.com>
2024-05-11 22:49:49 -0230

    cors edited

--- core/settings.py
+++ core/settings.py
@@ -142,12 +142,16 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5500",
     "http://10.0.0.126:3000",
     "https://slliks4.vercel.app"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5500",
     "http://10.0.0.126:3000",
     "https://slliks4.vercel.app"
 ]
