d17e08259f219b23186d250f8979de6c9cd47bd3
skills nwokolo anthony <skillsnwokolo372@gmail.com>
2024-05-18 23:57:48 -0230

    vercel deployment

--- core/settings.py
+++ core/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -134,6 +135,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 UPLOADS_URL='uploads/'
 UPLOADS_ROOT=os.path.join(BASE_DIR, 'uploads')
 # Default primary key field type
